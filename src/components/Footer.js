22c05538f0a7c745e62335e945514b5adea5e6bb
Jenna Petersen <70805517+Jenna-P@users.noreply.github.com>
2023-02-10 17:34:45 +0100

    similar exercise

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Box, Typography } from '@mui/material';
 
-const Footer = () => {
-  return (
-    <div>
-      Footer
-    </div>
-  )
-}
 
-export default Footer
+const Footer = () => (
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Typography variant="h5" sx={{ fontSize: { lg: '18px', xs: '12px' } }} color="#AFAFAE" mt="41px" textAlign="center" pt="60px" pb="20px">Code My Dream _Jenna Petersen ❤️</Typography>
+  </Box>
+);
+
+export default Footer;
--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { Box, Typography } from '@mui/material';
+
+import ExerciseCard from './ExerciseCard';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import BodyPart from './BodyPart';
 import RightArrowIcon from '../assets/icons/right-arrow.png';
@@ -35,7 +37,7 @@ const HorizontalScrollbar = ({data, bodyParts, setBodyPart, bodyPart}) => {
         itemId={item.id || item}
         title={item.id || item}
         m="0 40px">
-            <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
+            {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
         </Box>
        )
        )}
